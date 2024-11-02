c7dc85ba435cee508181c84da2412941ca9391ce
Anwar Rahim <anwargithub@gmail.com>
2024-11-02 18:28:21 +0500

    new task about aria label

--- javaScript/aria-label/script.js
+++ javaScript/aria-label/script.js
@@ -1,6 +1,9 @@
 
 
 
-function clicksubmit(){
-
+function clickSubmit(){
+    const button = document.getElementById("button-btn")
+    
+    button.outerHTML = "<p id='submit-form' class='sub-message-text'> Message Sent! ✅</p>"
 }
+
