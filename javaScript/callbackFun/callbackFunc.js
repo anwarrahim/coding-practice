da9e18e41e4cd7bcabd27466b50d38af9cd42097
Anwar Rahim <72375327+anwarrahim@users.noreply.github.com>
2025-03-04 20:15:07 +0000

    Add initial implementation of callback functions for user greeting

---
+++ javaScript/callbackFun/callbackFunc.js
@@ -0,0 +1,15 @@
+// working on callback function
+function userName(name){
+    console.log(name)
+}
+
+function greeting(greet){
+    console.log('Hello, how are you', greet)
+}
+
+function func(callback){
+
+}
+
+
+func()
